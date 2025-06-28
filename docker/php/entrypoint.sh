38b328bb0ea73b0394dab805f78b06eb730abfc3
devmaster178 <devmaster178@gmail.com>
2025-06-28 14:59:03 +0100

    Completed the initial docker setup

---
+++ docker/php/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+echo "⏳ Installing PHP dependencies..."
+composer install
+
+echo "⏳ Installing JS dependencies..."
+npm install
+
+echo "🔧 Building Tailwind CSS..."
+npm run build
+
+echo "🛠️ Waiting for DB..."
+until php bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
+  sleep 2
+done
+
+echo "📦 Creating database..."
+php bin/console doctrine:database:create --if-not-exists
+
+#echo "📜 Running migrations..."
+#php bin/console doctrine:migrations:migrate --no-interaction
+
+echo "🚀 Starting PHP-FPM..."
+exec php-fpm
