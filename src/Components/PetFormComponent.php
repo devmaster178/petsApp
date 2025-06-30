b41708e39cae9bbf24fecbe453b2fc3aebe23fd6
devmaster178 <devmaster178@gmail.com>
2025-06-30 20:34:07 +0100

    Refactored

--- src/Components/PetFormComponent.php
+++ src/Components/PetFormComponent.php
@@ -3,11 +3,8 @@ namespace App\Components;
 
 use App\Entity\Pet;
 use App\Form\PetFormType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
-use Symfony\UX\LiveComponent\Attribute\LiveAction;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
@@ -28,22 +25,4 @@ class PetFormComponent extends AbstractController
     {
         return $this->createForm(PetFormType::class, $this->initialFormData);
     }
-
-
-//    #[LiveAction]
-//    public function save(EntityManagerInterface $entityManager): RedirectResponse
-//    {
-//        // Submit the form! If validation fails, an exception is thrown
-//        // and the component is automatically re-rendered with the errors
-//        $this->submitForm();
-//
-//        /** @var Pet $post */
-//        $post = $this->getForm()->getData();
-//        $entityManager->persist($post);
-//        $entityManager->flush();
-//
-//        $this->addFlash('success', 'Post saved!');
-//
-//        return $this->redirectToRoute('app_get_breeds');
-//    }
 }
